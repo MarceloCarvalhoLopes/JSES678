67ce2847ad83f27c170778638b7830e6f81499e1
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2022-10-07 21:31:27 -0300

    destructuring

---
+++ destructuring.js
@@ -0,0 +1,12 @@
+var user = {
+    nome:"José",
+    profissao:"Programador",
+    idade:"Guia do programador",
+    curso: "Node.js"
+}
+
+var { nome, profissao } = user;
+//var nome = user.nome;
+
+console.log(nome);
+console.log(profissao);
